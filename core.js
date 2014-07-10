d11bd010003520ff1c20f68689af23ed26c8d633
Homletmoo <samlhub@gmail.com>
2014-07-10 23:16:57 +0100

    Tabs -> spaces.

--- core.js
+++ core.js
@@ -1,83 +1,92 @@
 function place_sections() {
-	var window_width = $(window).width();
-	$(".section").each(function(index) {
-		var width = $(this).width();
-		$(this).css(
-			"left",
-			(window_width - width) / 2 + window_width * index
-		);
-	});
+    var window_width = $(window).width();
+    $(".section").each(function(index) {
+        var width = $(this).width();
+        $(this).css(
+            "left",
+            (window_width - width) / 2 + window_width * index
+        );
+    });
 }
 
-/*function list_suggestions() {
-	// Populate object of suggestion types.
-	var suggestions = {};
-	$("a[data-fault]").each(function() {
-		var fault = $(this).attr("data-fault");
-		if (!suggestions.hasOwnProperty(fault)) {
-			suggestions[fault] = 0;
-		}
-	});
-	console.log(suggestions);
-}*/
+function list_suggestions(route) {
+    // Populate object of suggestion types.
+    faults = {};
+    route.forEach(function(entry) {
+        entry = entry[1];
+        $("#suggestions li[data-fault=" + entry + "]").css("display", "initial");
+        if (entry != "") {
+            if (faults.hasOwnProperty(entry)) {
+                faults[entry]++;
+            } else {
+                faults[entry] = 1
+            }
+        }
+    });
+    console.log(faults);
+}
 
 $(window).load(function() {
-	window.location.hash = "#home";
-	
-	// Populate list of section names.
-	var sections = Array();
-	$(".section").each(function() {
-		sections.push($(this).attr("data-name"));
-	});
-	
-	// Breadcrumb trail allows us to trace our steps back.
-	var route = Array();
-	route.push([0, ""]);
-	
-	$(".section a").click(function(event) {
-		// Prevent the browser from following the link.
-		event.preventDefault();
-		
-		// Get the index of the section.
-		var section = $(this).attr("href").replace("#", "");
-		var index = sections.indexOf(section);
-		
-		// If the section was "back", get the previous section.
-		if (index === -1) {
-			if (route.length > 1) { route.pop() };
-			index = route[route.length-1];
-		} else {
-			var fault = "";
-			if ($(this).is("[data-fault]")) {
-				fault = $(this).attr("data-fault");
-			}
-			route.push([index, fault]);
-		}
-		
-		// Update the hash.
-		window.location.hash = "#" + section;
-		
-		// Scroll to the section.
-		var window_width = $(window).width();
-		$("html").animate({
-				scrollLeft: index * window_width
-			},
-			400
-		);
-	});
-	
-	// Position the sections based on window size.
-	place_sections();
+    window.location.hash = "#home";
+    
+    // Populate list of section names.
+    var sections = Array();
+    $(".section").each(function() {
+        sections.push($(this).attr("data-name"));
+    });
+    
+    // Breadcrumb trail allows us to trace our steps back.
+    var route = Array();
+    route.push([0, ""]);
+    
+    $(".section a").click(function(event) {
+        // Prevent the browser from following the link.
+        event.preventDefault();
+        
+        // Get the index of the section.
+        var section = $(this).attr("href").replace("#", "");
+        var index = sections.indexOf(section);
+        
+        // If the section was "back", get the previous section.
+        if (index === -1) {
+            if (route.length > 1) { route.pop() };
+            index = route[route.length-1][0];
+        } else {
+            var fault = "";
+            if ($(this).is("[data-fault]")) {
+                fault = $(this).attr("data-fault");
+            }
+            route.push([index, fault]);
+            
+            if (section === "suggestions") {
+                list_suggestions(route);
+            }
+        }
+        
+        // Update the hash.
+        window.location.hash = "#" + section;
+        
+        // Scroll to the section.
+        var window_width = $(window).width();
+        $("html").animate({
+                scrollLeft: index * window_width
+            },
+            400
+        );
+    });
+    
+    // Position the sections based on window size.
+    place_sections();
 });
 
 $(window).unload(function() {
-	// Reset to beginning on page reload.
-	$("html").scrollLeft(0);
+    // Reset to beginning on page reload.
+    $("html").scrollLeft(0);
 });
 
 $(window).resize(place_sections);
 
 $(window).scroll(function() {
-	// Create background parallax effect.
-	$("html").css("background-position", $(window).scrollLeft()*0.8 + "px 0");
+    // Create background parallax effect.
+    $("html").css("background-position", $(window).scrollLeft()*0.8 + "px 0");
 });
